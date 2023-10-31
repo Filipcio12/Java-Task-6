fcf29413e166fa4d51071ba1d5eadb924d8fd3d5
Filip Fajfer <filipfajfer5@gmail.com>
2023-10-31 15:26:18 +0100

    Deleted some comments

--- TelephoneNumber.java
+++ TelephoneNumber.java
@@ -14,28 +14,7 @@ public class TelephoneNumber implements Comparable<TelephoneNumber> {
     }
 
     public int compareTo(TelephoneNumber oTelephoneNumber) {
-        /*
-        try {
-            int a = Integer.parseInt(countryCode);
-            int b = Integer.parseInt(oTelephoneNumber.countryCode);
-            
-            if (a != b) {
-                return a - b;
-            }
-
-            a = Integer.parseInt(localNumber);
-            b = Integer.parseInt(oTelephoneNumber.localNumber);
-
-            return a - b;
-        }
-        catch (NumberFormatException ex) {
-            System.out.println("Can't convert string to int.");
-            return 0;
-        }
-        */
-        
         int diff = countryCode.compareTo(oTelephoneNumber.countryCode);
-
         if (diff != 0) {
             return diff;
         }
